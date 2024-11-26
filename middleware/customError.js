f0a23b537af2827776a990779fa0c7f07395531e
Chaitanya Dhumal <chaitanya111203@gmail.com>
2024-11-26 23:02:33 +0530

    Custom Handler Error handeled

--- middleware/customError.js
+++ middleware/customError.js
@@ -1,8 +1,8 @@
-class customError extends Error {
+class CustomError extends Error {
     constructor(msg, status) {
       super(msg);
       this.status = status;
     }
 }
 
-module.exports = customError;
+module.exports = CustomError;
